d9fad69d6c8d2221e675a74e1d8034f5ff9b579f
Joris Noordermeer <joris@noordermeer.ch>
2026-01-11 06:13:12 +1300

    Initial Commit

---
+++ Harvester/HarvesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarvesterApp.swift
+//  Harvester
+//
+//  Created by Jorge on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HarvesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
