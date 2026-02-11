422c2aeb0e7c1bee369bf60ee2d05d32608c1c9e
Joris Noordermeer <joris@noordermeer.ch>
2026-02-11 05:38:36 +0100

    chore: switch AppUpdater to jorisnoo fork v3.0.0 and remove PromiseKit dependency

--- HarvestQRBill/AppDelegate.swift
+++ HarvestQRBill/AppDelegate.swift
@@ -6,24 +6,37 @@
 import AppKit
 #if !APP_STORE
 import AppUpdater
-import PromiseKit
 #endif
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     #if !APP_STORE
     let updater = AppUpdater(owner: "jorisnoo", repo: "HarvestQRBill")
-    
-    @objc func checkForUpdates() {
-        updater.check().catch(policy: .allErrors) { error in
-            if error.isCancelled {
-                let alert = NSAlert()
-                alert.messageText = "No Updates Available"
-                alert.informativeText = "You're running the latest version of HarvestQRBill."
-                alert.alertStyle = .informational
-                alert.addButton(withTitle: "OK")
-                alert.runModal()
+
+    func checkForUpdates() {
+        updater.check(
+            success: {
+                // Update found and downloaded - AppUpdater handles the flow
+            },
+            fail: { error in
+                Task { @MainActor in
+                    if let updateError = error as? AppUpdater.Error, case .noValidUpdate = updateError {
+                        let alert = NSAlert()
+                        alert.messageText = "No Updates Available"
+                        alert.informativeText = "You're running the latest version of HarvestQRBill."
+                        alert.alertStyle = .informational
+                        alert.addButton(withTitle: "OK")
+                        alert.runModal()
+                    } else {
+                        let alert = NSAlert()
+                        alert.messageText = "Update Check Failed"
+                        alert.informativeText = "Could not check for updates. Please try again later."
+                        alert.alertStyle = .warning
+                        alert.addButton(withTitle: "OK")
+                        alert.runModal()
+                    }
+                }
             }
-        }
+        )
     }
     #endif
 }
