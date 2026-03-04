6830100ce39f674d6d921ece80962fcde19c71dc
Joris Noordermeer <joris@noordermeer.ch>
2026-03-04 14:03:15 +0800

    feat: add automatic update check on app launch

--- HarvestQRBill/AppDelegate.swift
+++ HarvestQRBill/AppDelegate.swift
@@ -17,6 +17,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         repo: Bundle.main.infoDictionary?["GHRepositoryName"] as! String
     )
 
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        updater.check(
+            success: { [updater] in
+                Task { @MainActor in
+                    guard case .downloaded(let release, _, let bundle) = updater.state else { return }
+
+                    let alert = NSAlert()
+                    alert.messageText = "Update Available"
+                    alert.informativeText = "Version \(release.tagName) is ready to install."
+                    alert.alertStyle = .informational
+                    alert.addButton(withTitle: "Restart Now")
+                    alert.addButton(withTitle: "Later")
+
+                    if alert.runModal() == .alertFirstButtonReturn {
+                        do {
+                            try await updater.installThrowing(bundle)
+                        } catch {
+                            let errorAlert = NSAlert()
+                            errorAlert.messageText = "Update Failed"
+                            errorAlert.informativeText = error.localizedDescription
+                            errorAlert.alertStyle = .warning
+                            errorAlert.addButton(withTitle: "OK")
+                            errorAlert.runModal()
+                        }
+                    }
+                }
+            }
+        )
+    }
+
     func checkForUpdates() {
         updater.check(
             success: { [updater] in
